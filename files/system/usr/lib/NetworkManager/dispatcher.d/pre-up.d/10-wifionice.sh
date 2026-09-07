c829fb3389b0325a76218e2dbe989507edb9b7a0
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2026-09-07 10:50:36 +0000

    Always explicitly specify http method for wifi logons

--- files/system/usr/lib/NetworkManager/dispatcher.d/pre-up.d/10-wifionice.sh
+++ files/system/usr/lib/NetworkManager/dispatcher.d/pre-up.d/10-wifionice.sh
@@ -22,7 +22,7 @@ case "$CONNECTION_ID" in
     parms=('--data tncs=true' 'https://login.wifionice.de/auth_register_online')
     ;;
   WLAN@start)
-    parms=('https://start-unterelbe.on.icomera.com/cna/logon')
+    parms=('-X POST' 'https://start-unterelbe.on.icomera.com/cna/logon')
 	;;
   *)
     echo "nothing to do, exiting"
@@ -32,8 +32,8 @@ esac
 
 if [[ -n "${parms[@]}" ]]; then
 	for ((i = 0; i < 5; i++)); do
-		echo "$ curl --fail --verbose -X POST ${parms[@]}"
-		if curl --fail --verbose -X POST ${parms[@]}; then
+		echo "$ curl --fail --verbose ${parms[@]}"
+		if curl --fail --verbose ${parms[@]}; then
 			break
 		fi
 	done
